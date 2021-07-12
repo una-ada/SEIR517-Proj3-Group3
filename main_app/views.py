031c4d6d0fbe470d090619183f4b042a38bd9051
Muhammad Durrani <bbangdurrani@muhammads-mbp.home>
2021-07-13 06:34:31 +1000

    add feat homepage and style for homepage

--- main_app/views.py
+++ main_app/views.py
@@ -1,14 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-<<<<<<< HEAD
 from django.http import HttpResponse
 
 # Define the home view
 def home(request):
-  return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+  return render(request, 'about.html')
 
 def about(request):
   return render(request, 'about.html')
-=======
->>>>>>> parent of 90395f5 (fix: revert PR #3 merge (#4))
