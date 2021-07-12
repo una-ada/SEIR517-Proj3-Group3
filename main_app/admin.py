7d6558aac1819015c55515feba282a93dd606619
spencer brake <spencerbrake98@gmail.com>
2021-07-12 16:19:20 -0700

    fix(model) register models in admin.py

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Trip, Photo, Diary_Entry, Notes
 
 # Register your models here.
+admin.site.register(Trip)
+admin.site.register(Photo)
+admin.site.register(Diary_Entry)
+admin.site.register(Notes)
