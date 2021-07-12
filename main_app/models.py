b7215bc3c6c93b8e329bb519947cc137003df25e
spencer brake <spencerbrake98@gmail.com>
2021-07-12 14:24:11 -0700

    feat(model): add photo

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.fields import CharField
 
 # Create your models here.
 
@@ -8,3 +9,7 @@ class Trip(models.Model):
     from_location = models.CharField(max_length=100)
     to_location = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Photo(models.Model):
+    trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
+    image = CharField(max_length=200)
