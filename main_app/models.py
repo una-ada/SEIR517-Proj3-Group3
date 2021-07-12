ebf9f5590595ae0e2cb9e543454e4f22cdfb9984
spencer brake <spencerbrake98@gmail.com>
2021-07-12 15:49:53 -0700

    feat(model) add Notes

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.fields import CharField
+from django.db.models.fields import CharField, TextField
 
 # Create your models here.
 
@@ -17,3 +17,8 @@ class Photo(models.Model):
 class Diary_Entry(models.Model):
     trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
     content = models.TextField()
+
+class Notes(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
+    content = TextField()
