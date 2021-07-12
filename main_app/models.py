955ddc5d49525ab2da055689c9bfdade05c213e0
spencerbrake <80608618+spencerbrake@users.noreply.github.com>
2021-07-12 13:27:34 -0700

    feat(model): add trip (#7)

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Trip(models.Model):
+    title = models.CharField(max_length=100)
+    from_location = models.CharField(max_length=100)
+    to_location = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
