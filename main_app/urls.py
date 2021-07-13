0df56bc1354e7e16e0c0e4279096901a456a42ef
Una <trewbot@gmail.com>
2021-07-13 11:01:11 -0700

    fix(route): remove about

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.about, name='about')
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -5,7 +5,5 @@ from django.http import HttpResponse
 
 # Define the home view
 def home(request):
-  return render(request, 'about.html')
+  return render(request, 'home.html')
 
-def about(request):
-  return render(request, 'about.html')
