db4f38edc7dc376c141d65e818225174dec529e0
Una <trewbot@gmail.com>
2021-07-13 12:16:03 -0700

    feat(ctrl): add account signup

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('/accounts/signup/', views.signup, name='signup'),
+    path('accounts/signup/', views.signup, name='signup'),
     path('trips/new/', views.trips_create, name='trips_create'),
     path('trips/delete/', views.trips_delete, name='trips_delete'),
 ]
