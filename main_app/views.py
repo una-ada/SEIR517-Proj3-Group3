dbb8bc7a3cdffd489bf4a30f8cc070b7b7224b56
Una <trewbot@gmail.com>
2021-07-13 11:10:48 -0700

    fix(route): connect trips views (#30)
    * fix(route): remove about
    
    * feat(route): add trips_create
    
    * feat(route): add trips_delete

--- main_app/views.py
+++ main_app/views.py
@@ -5,7 +5,10 @@ from django.http import HttpResponse
 
 # Define the home view
 def home(request):
-  return render(request, 'about.html')
+  return render(request, 'home.html')
 
-def about(request):
-  return render(request, 'about.html')
+def trips_create(request):
+  return render(request, 'main_app/trip_form.html')
+
+def trips_delete(request):
+  return render(request, 'main_app/trip_confirm_delete.html')
