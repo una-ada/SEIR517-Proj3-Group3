db2ae4795aeef8f3952e4924227d72f506f907cb
bbangll <40874837+bbangll@users.noreply.github.com>
2021-07-14 13:57:47 +1000

    feat(ctrl): add trips controllers (#36)
    * added css file
    
    * pulling the latest file
    
    * add feat homepage and style for homepage
    
    * feat homepage,detailpage and createtripform style homepage, detailpage and createtripform
    
    Co-authored-by: Muhammad Durrani <bbangdurrani@muhammads-mbp.home>

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.fields import CharField, TextField
+from django.urls import reverse
 
 # Create your models here.
 
@@ -8,10 +9,13 @@ class Trip(models.Model):
     title = models.CharField(max_length=100)
     from_location = models.CharField(max_length=100)
     to_location = models.CharField(max_length=100)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    #user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title   
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'trip_id': self.id})
 
 class Photo(models.Model):
     trip = models.ForeignKey(Trip, on_delete=models.CASCADE)
