7bf949c4cd7c04c825a7a0e9682a2226d5f0a1cf
Una <trewbot@gmail.com>
2021-07-13 23:47:55 -0700

    feat(ctrl): add login required to trip CUD ops

--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Trip
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 #from django.views.generic import ListView, DetailView
 
-class TripCreate(CreateView):
+
+class TripCreate(LoginRequiredMixin, CreateView):
   model = Trip
   # This can't be '__all__', probably because the form is missing the user
   # field which needs to be added by the validation method!
@@ -15,11 +17,13 @@ class TripCreate(CreateView):
     form.instance.user = self.request.user
     return super().form_valid(form)
 
-class TripUpdate(UpdateView):
+
+class TripUpdate(LoginRequiredMixin, UpdateView):
   model = Trip
   fields = '__all__'
 
-class TripDelete(DeleteView):
+
+class TripDelete(LoginRequiredMixin, DeleteView):
   model = Trip
   success_url = '/'
 
