0fb31dcc4fb1b814c48cac49d33f511bd4ae3ba8
Una <trewbot@gmail.com>
2021-07-13 23:42:57 -0700

    feat(ctrl): add current user to trip create

--- main_app/views.py
+++ main_app/views.py
@@ -5,31 +5,34 @@ from .models import Trip
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 #from django.views.generic import ListView, DetailView
 
-
-
 class TripCreate(CreateView):
-    model = Trip
-    fields = '__all__'
+  model = Trip
+  # This can't be '__all__', probably because the form is missing the user
+  # field which needs to be added by the validation method!
+  fields = ['title', 'from_location', 'to_location']
+  
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super().form_valid(form)
 
 class TripUpdate(UpdateView):
-    model = Trip
-    fields = '__all__'
+  model = Trip
+  fields = '__all__'
 
 class TripDelete(DeleteView):
-    model = Trip
-    success_url = '/'
-
+  model = Trip
+  success_url = '/'
 
 # Define the home view
 # Create your views here.
 
 def trips_index(request):
   trips = Trip.objects.all()
-  return render(request, 'home.html', {'trips': trips })
+  return render(request, 'home.html', {'trips': trips})
 
-def trips_detail(request,trip_id):
-    trip = Trip.objects.get(id=trip_id)
-    return render(request, 'trips/detail.html', {'trip': trip})
+def trips_detail(request, trip_id):
+  trip = Trip.objects.get(id=trip_id)
+  return render(request, 'trips/detail.html', {'trip': trip})
 
 def signup(request):
   error_message = ''
