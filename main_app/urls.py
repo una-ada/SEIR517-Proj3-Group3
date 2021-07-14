2e849778610bd8f37fee674cf1c9790c5db72d44
Hanna Nordstrom <hjamienordstrom@Hannas-MacBook-Pro.local>
2021-07-13 20:25:56 -0400

    broken render

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('trips/', views.trips_index, name='index'),
+    path('trips/<int:trip_id>/', views.trips_detail, name='detail'),
     path('accounts/signup/', views.signup, name='signup'),
     path('trips/new/', views.trips_create, name='trips_create'),
     path('trips/delete/', views.trips_delete, name='trips_delete'),
