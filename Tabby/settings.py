b64b78663d3966c47e38268c23310b455d01c038
Una <trewbot@gmail.com>
2021-07-15 12:04:03 -0700

    Revert "Merge branch 'main' into closebutton"
    This reverts commit 25d26e0650c808b8739e21a7b5dd9db5607f13db, reversing
    changes made to 820aaeec4e6d23ad9bc9b8b53fc470f3739cbd8a.

--- Tabby/settings.py
+++ Tabby/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,5 +125,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
-
-django_heroku.settings(locals())
