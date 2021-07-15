e49ee3700f2a0bb3aab0fc28e0e83a885159a1f6
Una <trewbot@gmail.com>
2021-07-14 21:44:30 -0700

    lint(route): lint urls.py

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,12 +5,31 @@ urlpatterns = [
     path('', views.trips_index, name='index'),
     path('trips/<int:trip_id>/', views.trips_detail, name='detail'),
     path('trips/create/', views.TripCreate.as_view(), name='trips_create'),
-    path('trips/<int:pk>/update/', views.TripUpdate.as_view(), name='trips_update'),
-    path('trips/<int:pk>/delete/', views.TripDelete.as_view(), name='trips_delete'),
-    path('trips/<int:trip_id>/add_diary_entry/', views.add_diary_entry, name='add_diary_entry'),
+    path(
+        'trips/<int:pk>/update/',
+        views.TripUpdate.as_view(),
+        name='trips_update'
+    ),
+    path(
+        'trips/<int:pk>/delete/',
+        views.TripDelete.as_view(),
+        name='trips_delete'
+    ),
+    path(
+        'trips/<int:trip_id>/add_diary_entry/',
+        views.add_diary_entry,
+        name='add_diary_entry'
+    ),
     path('trips/<int:trip_id>/add_note/', views.add_note, name='add_note'),
-    path('diary/<int:pk>/update/', views.DiaryUpdate.as_view(), name='diary_update'),
-    path('diary/<int:pk>/delete/', views.DiaryDelete.as_view(), name='diary_delete'),
+    path(
+        'diary/<int:pk>/update/',
+        views.DiaryUpdate.as_view(),
+        name='diary_update'
+    ),
+    path(
+        'diary/<int:pk>/delete/',
+        views.DiaryDelete.as_view(),
+        name='diary_delete'
+    ),
     path('accounts/signup/', views.signup, name='signup'),
 ]
-
